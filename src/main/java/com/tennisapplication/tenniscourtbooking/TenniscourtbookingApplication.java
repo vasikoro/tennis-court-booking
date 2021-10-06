963879a61c7a52155ddbaab278522d3c46d92fc0
antzelika <aggeliki.kouneli@gmail.com>
2021-10-06 08:45:43 +0300

    new files for project

---
+++ src/main/java/com/tennisapplication/tenniscourtbooking/TenniscourtbookingApplication.java
@@ -0,0 +1,13 @@
+package com.tennisapplication.tenniscourtbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenniscourtbookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenniscourtbookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tennisapplication/tenniscourtbooking/TenniscourtbookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tennisapplication.tenniscourtbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenniscourtbookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
